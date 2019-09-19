74e500d6f87da6c7aa66002e6a5424e6b18c3eb4
Anthony Fojas <anthonyjfojas@gmail.com>
2019-09-18 21:46:55 -0700

    Seed Cassandra database with csv file

--- server/db/index-psql.js
+++ server/db/index-psql.js
@@ -1,3 +1,4 @@
+const faker = require('faker');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,9 +11,13 @@ const pool = new Pool({
 pool.connect();
 
 pool.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message); // Hello World!
-  // pool.end();
+  console.log(err ? err.stack : res.rows[0].message);
 });
+
+const randomInt = function (max) {
+  return Math.ceil(Math.random() * max);
+};
+const tenMil = 10000000;
 
 const getListingPhotos = function (listingID, callback) {
   pool.query(`SELECT * FROM photos WHERE listingID = ${listingID}`, (error, results, fields) => {
@@ -24,15 +29,15 @@ const getListingPhotos = function (listingID, callback) {
   });
 };
 
-// const postListingPhoto = function (listingID, callback) {
-//   pool.query(`SELECT * FROM photos WHERE listingID = ${listingID}`, (error, results, fields) => {
-//     if (error) {
-//       callback(error);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
+const postListingPhotos = function (listingID, callback) {
+  pool.query(`INSERT INTO photos VALUES (default, https://bnbair.s3-us-west-1.amazonaws.com/${randomInt(100)}.jpg, ${faker.lorem.sentence()}, ${randomInt(tenMil)}`, (error, results, fields) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, results);
+    }
+  });
+};
 
 // const getListingPhotos = function (listingID, callback) {
 //   pool.query(`SELECT * FROM photos WHERE listingID = ${listingID}`, (error, results, fields) => {
